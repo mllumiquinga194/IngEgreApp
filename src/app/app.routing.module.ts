b80536e1692f00733bb75a74459b50a5c663d676
Marcos <mallth194@gmail.com>
2018-09-07 21:34:11 -0400

    Interfaz COmpleta

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from "@angular/core";
+import { Routes, Router, RouterModule } from "@angular/router";
+import { LoginComponent } from "./auth/login/login.component";
+import { RegisterComponent } from "./auth/register/register.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { dashboardRoutes } from "./dashboard/dashboard.routes";
+
+
+const routes: Routes = [
+    { path: 'login', component:LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { 
+        path: '', 
+        component: DashboardComponent, //asi defino rutas hijas.
+        children: dashboardRoutes
+    },
+    { path: '**', redirectTo: '' }
+];
+@NgModule({
+
+    imports:[
+        RouterModule.forRoot( routes )
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule{
+
+}
